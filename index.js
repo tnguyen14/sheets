50324456f44105d2d70dfbbf5d7db044dc5bead6
Tri Nguyen <tri@tridnguyen.com>
2026-04-21 00:51:46 +0000

    Remove unused AUTH0 env vars, as API currently do not check auth

--- index.js
+++ index.js
@@ -1,8 +1,4 @@
-require("dotenv").config();
-
 const server = require("@tridnguyen/fastify-server")({
-  auth0Domain: process.env.AUTH0_DOMAIN,
-  auth0ClientId: process.env.AUTH0_CLIENT_ID,
   allowedOrigins: ["https://lab.tridnguyen.com", "https://tridnguyen.com"],
   shouldPerformJwtCheck: false,
 });
